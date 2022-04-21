f48b41a9416469b135daf1d168e2ff43feca727e
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-04-21 18:53:39 +1200

    chore(front-end): connect to API (WIP)

--- frontend/components/SongItem.tsx
+++ frontend/components/SongItem.tsx
@@ -2,22 +2,32 @@ type SongItemProps = {
   index: number
   title: string
   cover: string
-  duration: string
+  duration: number
+  onClick: () => void
 }
 
-const SongItem = ({ index, title, cover, duration }: SongItemProps) => {
+const SongItem = ({
+  index,
+  title,
+  cover,
+  duration,
+  onClick,
+}: SongItemProps) => {
+  const mind = duration % (60 * 60)
+  const minutes = Math.floor(mind / 60)
+  const seconds = Math.ceil(mind % 60)
   return (
-    <div className="flex flex-row items-center gap-6">
+    <div className="flex flex-row items-center gap-6" onClick={() => onClick()}>
       <div className="w-8 text-gray-600">
         {index > 10 ? index : `0${index}`}
       </div>
       <img
         src={cover}
         alt={title}
-        className="object-contain w-8 h-8 rounded aspect-square"
+        className="object-cover w-12 h-12 rounded aspect-square"
       />
       <div className="flex-1 truncate">{title}</div>
-      <div>{duration}</div>
+      <div>{`${minutes}:${seconds}`}</div>
     </div>
   )
 }
--- frontend/pages/playlist/[id].tsx
+++ frontend/pages/playlist/[id].tsx
@@ -1,29 +1,92 @@
 import type { NextPage } from 'next'
 import SongItem from '../../components/SongItem'
 import PlaylistHeader from '../../components/PlaylistHeader'
-import { getPlayList, getPlayListInfo } from '../../mock/playlist'
+import { useEffect, useState } from 'react'
+import YouTube from 'react-youtube'
+
+type singleSong = {
+  videaId: string
+  title: string
+  cover: string
+  duration: number
+}
+
+type info = {
+  title: string
+  cover: string
+  owner: string
+  description: string
+}
 
 const Playlist: NextPage = () => {
-  const songs = getPlayList()
-  const info = getPlayListInfo()
+  const [playlist, setPlaylist] = useState<singleSong[]>([])
+  const [playlistInfo, setPlaylistInfo] = useState<info>()
+  const [currentSong, setCurrentSong] = useState<string>()
+
+  useEffect(() => {
+    fetch('/api/playlist')
+      .then((res) => res.json())
+      .then((res: any) => {
+        setPlaylistInfo({
+          title: res.title,
+          cover: res.thumbnails[1].url, // use the medium size cover
+          owner: res.owner,
+          description: res.dateYear, // todo need a actual description
+        })
+        const list = res.content.map((song: any) => {
+          return {
+            videaId: song.videoId,
+            title: song.name,
+            cover:
+              song.thumbnails.url ||
+              'https://www.mariposakids.co.nz/wp-content/uploads/2014/08/image-placeholder2.jpg', // image placeholder
+            duration: song.duration,
+          }
+        })
+        setPlaylist(list)
+      })
+  }, [])
+
+  const opts = {
+    height: '200',
+    width: '400',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  }
+
   return (
     <>
       <div className="flex flex-col w-full max-w-screen-xl gap-8 px-6 mx-auto my-24">
         {/* header section */}
-        <PlaylistHeader {...info} />
+        {/* todo need to add loading animation */}
+        {playlistInfo ? <PlaylistHeader {...playlistInfo} /> : null}
         {/* playlist section */}
         {/* todo this bg color need to be updated */}
         <div className="flex flex-col gap-4 px-12 py-8 rounded-2xl bg-slate-50">
-          {songs.map((item, i) => (
+          {playlist.map((item, i) => (
             <SongItem
               key={i}
               index={i + 1}
               title={item.title}
               cover={item.cover}
               duration={item.duration}
+              onClick={() => {
+                setCurrentSong(item.videaId)
+                console.log(item.videaId)
+              }}
             />
           ))}
         </div>
+        {/* only used for testing */}
+        {currentSong ? (
+          <YouTube
+            videoId={currentSong}
+            opts={opts}
+            className="fixed right-12 top-16"
+          />
+        ) : null}
       </div>
     </>
   )
