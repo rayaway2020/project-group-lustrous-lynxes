ebbf74aa3d3406755874db4d6e5febf83e9ea2ff
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-05-05 19:42:13 +1200

    feat: import daisyui

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,18 +1,21 @@
 import type { NextPage } from 'next'
-import React, {useState} from 'react';
+import React, { useState } from 'react'
 
 const search: NextPage = () => {
   return (
-    <div className = "flex flex-col text-center w-full max-w-screen-xl gap-20 pt-28 pb-96 mx-auto my-24 rounded-3xl drop-shadow-xl bg-[url('https://cdn.pixabay.com/photo/2017/11/11/18/31/music-player-2939936_1280.jpg')]">
-        <div>        
-          <h1 className = "text-[32px] font-bold">Play over 90 million songs and</h1>
-          <h1 className = "text-[32px] font-bold">30000 playlists</h1>
-        </div>
-        <form className="searchForm">  
-            <input id="searchText" type="text"/>‘
-            <button>Search</button>
-        </form>
+    <div className="mx-auto my-24 flex w-full max-w-screen-xl flex-col gap-20 rounded-3xl bg-[url('https://cdn.pixabay.com/photo/2017/11/11/18/31/music-player-2939936_1280.jpg')] pt-28 pb-96 text-center drop-shadow-xl">
+      <div>
+        <h1 className="text-[32px] font-bold">
+          Play over 90 million songs and
+        </h1>
+        <h1 className="text-[32px] font-bold">30000 playlists</h1>
+      </div>
+      <input
+        type="text"
+        placeholder="Type here"
+        className="w-full max-w-xs input input-bordered"
+      />
     </div>
   )
 }
-export default search;
+export default search
