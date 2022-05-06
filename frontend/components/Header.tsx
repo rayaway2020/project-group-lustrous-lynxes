ff5f95fb850c01002730396b06cac1d54f6149a8
rayaway2020 <69098495+rayaway2020@users.noreply.github.com>
2022-05-06 23:07:08 +1200

    Pop up for login

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,4 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import React from 'react';
+
 
 const Header = () => {
   const goBack = () => {
@@ -10,7 +12,6 @@ const Header = () => {
   const toHomepage = () => {}
   const toDiscover = () => {}
   const toPlaylist = () => {}
-  const toLogIn = () => {}
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 flex max-w-screen-xl px-6 py-4 m-auto bg-white">
@@ -40,12 +41,28 @@ const Header = () => {
       {/* search bar and avatar */}
       <div className="flex flex-row items-center justify-end flex-1 w-0 gap-4">
         <div className="avatar">
-          <div className="w-12 rounded-full" >
-            <img src="https://api.lorem.space/image/face?hash=47449" onClick={() => toLogIn()}/>
+          {/* <div className="w-12 rounded-full" >
+            <img for="my-modal" className="cursor-pointer" src="https://api.lorem.space/image/face?hash=47449"/>
+          </div> */}
+          <label for="my-modal" class="btn modal-button w-12 rounded-full" >
+            <img for="my-modal" className="w-12" src="https://api.lorem.space/image/face?hash=47449"/>
+          </label>
+        </div>
+      </div>
+      <input type="checkbox" id="my-modal" class="modal-toggle" />
+
+      <div class="modal">
+        <div class="modal-box">
+          <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+          <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+          <div class="modal-action">
+            <label for="my-modal" class="btn">Yay!</label>
           </div>
         </div>
       </div>
     </header>
   )
 }
+
+
 export default Header
--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import React, { useState } from 'react'
 
 const search: NextPage = () => {
   return (
@@ -11,7 +10,7 @@ const search: NextPage = () => {
         <h1 className="text-[32px] font-bold">30000 playlists</h1>
       </div>
       <div className="form-control">
-        <div className="input-group-center">
+        <div className="input-group flex items-center justify-center w-full">
           <input type="text" placeholder="Search…" className="input input-bordered w-3/5" />
           <button className="btn btn-square">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
