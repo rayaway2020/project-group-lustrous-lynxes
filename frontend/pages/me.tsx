d5fc1847837bf5e7ac3e98824739bc667e69cab3
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-05-09 17:15:32 +1200

    ref: playlist/index -> me

---
+++ frontend/pages/me.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import PlaylistLikedSongs from '../components/PlaylistLikedSongs'
+
+const Home: NextPage = () => {
+  return <PlaylistLikedSongs />
+}
+
+export default Home
