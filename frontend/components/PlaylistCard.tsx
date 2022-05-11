46b350c0c48615efbce8d3451802af869d6a146d
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-05-12 00:18:20 +1200

    chore: fix playlist card

--- frontend/components/PlaylistCard.tsx
+++ frontend/components/PlaylistCard.tsx
@@ -4,33 +4,42 @@ import axios from 'axios'
 import { useState } from 'react'
 
 interface PlaylistCardProps {
-  id: string
-  browseId: string // playlist id
+  id?: string
+  browseId?: string // playlist id
   cover: string
   title: string
   subtitle: string
 }
 
 //需要进一步考虑edge cases
-const PlaylistCard = ({ id, browseId, cover, title, subtitle }: PlaylistCardProps) => {
+// update: type of id and browseId
+const PlaylistCard = ({
+  id,
+  browseId,
+  cover,
+  title,
+  subtitle,
+}: PlaylistCardProps) => {
   const [playlistId, setPlaylistId] = useState(id)
   return (
     <div
       className="flex flex-col"
       onClick={() => {
-        browseId !== "" ?
-        axios.post("http://localhost:3001/api/playlists/public", {
+        browseId
+          ? axios
+              .post('http://localhost:3001/api/playlists/public', {
                 title: title,
                 thumbnail: cover,
                 author: subtitle,
-                browseId: browseId
-        }).then(res => {
-          setPlaylistId(res.data._id);
-          //如果点击外部来源的歌单
-          router.push(`/playlist/${browseId}/`);
-        }) :
-        //如果点击用户创造的歌单
-        router.push(`/playlist/${playlistId}/`);
+                browseId: browseId,
+              })
+              .then((res) => {
+                setPlaylistId(res.data._id)
+                //如果点击外部来源的歌单
+                router.push(`/playlist/${browseId}/`)
+              })
+          : //如果点击用户创造的歌单
+            router.push(`/playlist/${playlistId}/`)
       }}
     >
       <Cover url={cover} alt={title} />
@@ -41,5 +50,3 @@ const PlaylistCard = ({ id, browseId, cover, title, subtitle }: PlaylistCardProp
 }
 
 export default PlaylistCard
-
-
