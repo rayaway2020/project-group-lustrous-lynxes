de73c9983b382f7b031a830ea63ab3d2d90f097e
Jasmine Chen <jasminechen0621@gmail.com>
2022-05-11 20:02:42 +1200

    testexample

--- backend/src/routes/api/search.js
+++ backend/src/routes/api/search.js
@@ -39,4 +39,38 @@ router.get('/songs', async (req, res) => {
     });
 });
 
+router.get('/', async (req, res) => {
+    const searchQuery = req.query.search;
+
+    const result = {} 
+    const api = await getAPIInstance();
+    api.search(searchQuery, 'song').then(result => {
+        const raw = result.content.map(item => {
+            if (!item.thumbnails || item.thumbnails.length == 0) {
+                item.thumbnails = [
+                    {
+                        url: 'https://c.tenor.com/Tu0MCmJ4TJUAAAAC/load-loading.gif',
+                    },
+                ];
+            }
+        });
+    });    
+
+    api.search(searchQuery, 'playlist').then(result => {
+        console.log(result);
+        const raw = result.content.map(item => {
+            return {
+                browseId: item.browseId,
+                title: item.title,
+                author: item.author,
+                thumbnail:
+                    item.thumbnails[1]?.url ||
+                    'https://c.tenor.com/Tu0MCmJ4TJUAAAAC/load-loading.gif',
+            };
+        });
+        res.send(raw);
+    });
+
+})
+
 export default router;
