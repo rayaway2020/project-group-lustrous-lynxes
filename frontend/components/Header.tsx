eaca170347819c18e40118ab0507824859e9617b
rayaway2020 <69098495+rayaway2020@users.noreply.github.com>
2022-05-12 01:58:55 +1200

    MUI implementation of login/register validation

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -21,36 +21,40 @@ const Header = () => {
     useContext(userContext)
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
-  const initialValues = { username: '', email: '', password: '' }
-  const [formValues, setFormValues] = useState(initialValues)
+
+  const initialValues = { email: '', username: '', password: '' }
   const [formErrors, setFormErrors] = useState({
+    email: '',
     username: '',
     password: '',
-    email: '',
   })
 
   const [isRegister, setIsRegister] = useState(false)
-  const [isSubmit, setIsSubmit] = useState(false)
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const [showError, setShowError] = useState(false);
 
-  
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const handleOpen = () => setDialogOpen(true);
+  const handleClose = () => {
+    setDialogOpen(false);
+    resetValues()
+  }
+  const handleClickOpen = () => {
+    setDialogOpen(true);
+  };
+  const [emailError, setEmailError] = useState(false);
+  const [userNameError, setUserNameError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+// const style = {
+//   position: 'absolute' as 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 400,
+//   bgcolor: 'background.paper',
+//   border: '2px solid #000',
+//   boxShadow: 24,
+//   p: 4,
+// };
 
   const goBack = () => {
     history.back()
@@ -59,39 +63,69 @@ const style = {
     history.forward()
   }
 
-  useEffect(() => {
-    console.log(formErrors)
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues)
-    }
-  }, [formErrors])
+  const resetValues = () => {
+    setEmail('')
+    setUsername('')
+    setPassword('')
+    setEmailError(false)
+    setUserNameError(false)
+    setPasswordError(false)
+    setFormErrors(initialValues)
+  }
 
-  const validateRegister = (values: any) => {
-    const errors = { username: '', password: '', email: '' }
+  const switchDialog = () => {
+    resetValues()
+    setIsRegister(!isRegister)
+  }
+
+  const handleSubmit = () => {
+    setEmailError(false)
+    setUserNameError(false)
+    setPasswordError(false)
+    // console.log("new turn")
+    const errors = {email: '', username: '', password: ''}
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
 
-    if (!values.username) {
+    if(isRegister){
+      if (!email) {
+        errors.email = 'Email is required!'
+        setEmailError(true)
+      } else if (!regex.test(email)) {
+        errors.email = 'This is not a valid email format'
+        setEmailError(true)
+      }
+    }
+
+    if (!username) {
       errors.username = 'Username is required!'
+      setUserNameError(true)
     }
 
-    if (!values.password) {
+    if (!password) {
       errors.password = 'Password is required!'
-    } else if (values.password.length < 8) {
+      setPasswordError(true)
+    } else if (password.length < 8) {
       errors.password = 'Password must be at least 8 characters'
+            setPasswordError(true)
     }
 
-    if (!values.email) {
-      errors.email = 'Email is required!'
-    } else if (!regex.test(values.email)) {
-      errors.email = 'This is not a valid email format'
+    if (errors.email == '' && errors.username == '' && errors.password == ''){
+      if(isRegister){
+        register()
+      }else{
+        login()
+      }
+      console.log("yay")
+      handleClose()
+    }else{
+      setFormErrors(errors)
+      console.log(formErrors.email)
+      console.log(formErrors.username)
+      console.log(formErrors.password )
     }
-    return errors
   }
 
   const register = () => {
-    setFormErrors(validateRegister(formValues))
-    setIsSubmit(true)
-
     Axios.post('http://localhost:3001/api/auth/register', {
       username: username,
       password: password,
@@ -105,7 +139,7 @@ const style = {
       }
     })
   }
-
+  
   const login = () => {
     Axios.post('http://localhost:3001/api/auth/login', {
       username: username,
@@ -167,18 +201,17 @@ const style = {
           <img
             className="object-cover w-12 h-12 rounded-full"
             src="https://api.lorem.space/image/face?hash=47449"
+            onClick={handleOpen}
           />
         </label>
       </div>
       
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={dialogOpen} onClose={handleClose}>
         {isRegister? <DialogTitle>Register</DialogTitle> : <DialogTitle>Login</DialogTitle>}
         <DialogContent>
           
           {isRegister? <TextField
+            error = {emailError}
             autoFocus
             margin="dense"
             id="name"
@@ -186,10 +219,14 @@ const style = {
             type="email"
             fullWidth
             variant="standard"
+            value = {email}
+            onChange={(e) => setEmail(e.target.value)}
+            helperText={formErrors.email}
             required
           /> : null}
           
           <TextField
+            error={userNameError}
             autoFocus
             margin="dense"
             id="name"
@@ -197,194 +234,36 @@ const style = {
             type="text"
             fullWidth
             variant="standard"
+            onChange={(e) => setUsername(e.target.value)}
+            helperText={formErrors.username}
+            value = {username}
             required
           />
           <TextField
-            error={showError}
+            error={passwordError}
             autoFocus
             margin="dense"
             id="name"
             label="Password"
             type="password"
             fullWidth
-            onChange={(e) => setShowError(e.target.value.length <= 8)}
+            onChange={(e) => setPassword(e.target.value)}
             variant="standard"
             required
-            helperText="Password should be greater than 7"
+            helperText={formErrors.password}
+            value ={password}
           />
         </DialogContent>
         
-        {isRegister?(<DialogContentText onClick={() => setIsRegister(false)}>Click here to log in</DialogContentText>) : (<DialogContentText onClick={() => setIsRegister(true)}>Click here to register</DialogContentText>) }
+        {isRegister?(<DialogContentText fontWeight= 'bold' fontStyle= 'oblique' textAlign='center' color='red' onClick={switchDialog}>Already got an account? Click here to log in</DialogContentText>) 
+        : (<DialogContentText onClick={switchDialog}>Sign up for an new account?</DialogContentText>) }
         
-
-        
-
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit" onClick={handleClose}>Submit</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
         </DialogActions>
       </Dialog>
 
-      <input type="checkbox" id="logIn-modal" className="modal-toggle" />
-      <div className="modal">
-        <label
-          htmlFor="logIn-modal"
-          className="absolute btn btn-circle btn-sm right-2 top-2"
-        >
-          ✕
-        </label>
-        <div className="w-2/3 rounded-lg hero h-3/5 bg-base-200">
-          <div className="flex-col hero-content lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
-              <h1 className="text-5xl font-bold">Login now!</h1>
-              <p className="py-6">
-                Listening is everything, create an account to share your
-                thoughts to others all over the world
-              </p>
-            </div>
-
-            <div className="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
-              <div className="card-body">
-                <div className="form-control">
-                  <label className="label">UserName</label>
-                  <input
-                    type="text"
-                    placeholder="UserName"
-                    className="input input-bordered"
-                    minLength={6}
-                    onChange={(e) => {
-                      setUsername(e.target.value)
-                    }}
-                  />
-                </div>
-
-                <div className="form-control">
-                  <label className="label">Password</label>
-                  <input
-                    type="password"
-                    placeholder="password"
-                    className="input input-bordered"
-                    onChange={(e) => {
-                      setPassword(e.target.value)
-                    }}
-                  />
-                  <label className="label">
-                    <label
-                      htmlFor="register-modal"
-                      className="btn btn-link btn-xs"
-                      onClick={login}
-                    >
-                      Sign up for a new account?
-                    </label>
-                  </label>
-                </div>
-
-                <div className="mt-6 form-control">
-                  <label
-                    htmlFor="logIn-modal"
-                    className="btn btn-primary"
-                    onClick={login}
-                  >
-                    LOGIN
-                  </label>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <input type="checkbox" id="register-modal" className="modal-toggle" />
-      <div className="modal" id="register-modal">
-        <label
-          htmlFor="register-modal"
-          className="absolute btn btn-circle btn-sm right-2 top-2"
-        >
-          ✕
-        </label>
-
-        <div className="w-2/3 rounded-lg hero h-2/3 bg-base-200">
-          <div className="flex-col hero-content lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
-              <h1 className="text-5xl font-bold">Sign Up!</h1>
-              <p className="py-6">
-                Listening is everything, create an account to share your
-                thoughts to others all over the world
-              </p>
-            </div>
-            <div className="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
-              <div className="card-body">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">UserName</span>
-                  </label>
-                  <p className="text-xs text-red-600 underline label-text underline-offset-1">
-                    {formErrors.username}
-                  </p>
-                  <input
-                    type="text"
-                    placeholder="UserName"
-                    className="input input-bordered"
-                    value={formValues.username}
-                    onChange={(e) => {
-                      setUsername(e.target.value)
-                      formValues.username = e.target.value
-                    }}
-                  />
-                </div>
-
-                <div className="form-control">
-                  <label className="label">Password</label>
-                  <p className="text-xs text-red-600 underline label-text underline-offset-1">
-                    {formErrors.password}
-                  </p>
-                  <input
-                    type="password"
-                    placeholder="password"
-                    className="input input-bordered"
-                    value={formValues.password}
-                    onChange={(e) => {
-                      setPassword(e.target.value)
-                      formValues.password = e.target.value
-                    }}
-                  />
-                </div>
-
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <p className="text-xs text-red-600 underline label-text underline-offset-1">
-                    {formErrors.email}
-                  </p>
-                  <input
-                    type="text"
-                    placeholder="email"
-                    className="input input-bordered"
-                    value={formValues.email}
-                    onChange={(e) => {
-                      setEmail(e.target.value)
-                      formValues.email = e.target.value
-                    }}
-                  />
-                </div>
-                <div className="mt-6 form-control">
-                  {/* <label htmlFor="register-modal" className="btn btn-primary" onClick={register}>
-                    Register
-                  </label> */}
-                  <label className="btn btn-primary" onClick={register}>
-                    Register
-                  </label>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* <div>
-          {Object.keys(formErrors).length === 0 && isSubmit ? (<div className='ui message success'>Signed In successfully</div>
-          ) : (<div className='ui message success'>Not yet sign in</div>)}
-        </div> */}
-      </div>
     </header>
   )
 }
